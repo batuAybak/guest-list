fa0aee492d2e452b5d80898b1531fcde18a34471
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-18 10:10:13 -0400

    add comment

--- src/hooks/GetDataHooks.jsx
+++ src/hooks/GetDataHooks.jsx
@@ -2,6 +2,19 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function GetDataHooks(url) {
+  /**
+   * Initialized an empty array (useState([]))
+   * because we want components that need "guests" to know it's an array.
+   * That's because components render before useEffect()
+   * and since e.g. GuestList uses .map function, it should expect and array.
+   * It will be empty initialy, but .map execution at least won't fail
+   *
+   * GuestList renders first (fast),
+   * fetch happens asynchronously afterward (slower).
+   * The empty array ensures the first render doesn’t crash.
+   * …so that GuestList can safely render before the data arrives.
+   * Without this, calling .map() on undefined would throw an error.
+   */
   const [guests, setGuests] = useState([]);
   const [loading, setLoading] = useState(true);
 
